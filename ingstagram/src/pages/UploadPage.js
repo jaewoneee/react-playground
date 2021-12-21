538ebfa181081288daafd17fd01ec269365c5669
jaewoneee <jaewon810@naver.com>
2021-12-21 17:17:48 +0900

    feat: checking validation of file extension

--- ingstagram/src/pages/UploadPage.js
+++ ingstagram/src/pages/UploadPage.js
@@ -23,14 +23,34 @@ function Upload() {
 
 function setThumbnail(event) {
     const reader = new FileReader();
-    reader.onload = e => {
-        let img = document.querySelector(".uploaded-img");
-        img.setAttribute("src", e.target.result);
-        
+    const img = document.querySelector(".uploaded-img");
+    const fileExtension = {
+        name: event.target.files[0].name,
+        size:event.target.files[0].size
     };
+    
+    if (checkFileExtension(fileExtension)) {
+        reader.onload = e => {
+        img.setAttribute("src", e.target.result);
+     };
     reader.readAsDataURL(event.target.files[0]);
+    } else {
+        alert('Upload failed!');
+    }
+    
 }
 
+// 유효한 파일인지 체크
+function checkFileExtension(file) {
+    const regex = new RegExp("(.*?)\.(jpg|jpeg|png|bmp|gif)$"); // 이미지 파일만 허용
+    const maxSize = 5242880; //5MB 이하
+    
+    if (!regex.test(file.name) || file.size >= maxSize) {
+        return false;
+    } else {
+        return true;
+    }
 
+}
 
 export default Upload;
