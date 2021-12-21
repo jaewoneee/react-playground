2ec782c6d8083a286b4f45176731d2e64c32c46d
jaewoneee <jaewon810@naver.com>
2021-12-21 09:58:09 +0900

    feat: preview of uploaded image

--- ingstagram/src/pages/UploadPage.js
+++ ingstagram/src/pages/UploadPage.js
@@ -1,9 +1,36 @@
+import defaultThumb from '../assets/images/upload.png';
+    
 function Upload() {
+
     return (
-        <div className="upload-wrap">
-            uploadPage
-        </div>
+        <form>
+            <div className="upload-wrap">
+                <div className="image-box">
+                    <div className="upload-img-btn">
+                         <label htmlFor="upload"></label> 
+                         <input type="file" id="upload" accept="image/*" onChange={(e) => { setThumbnail(e);}}></input>
+                     </div>
+                    <img src={defaultThumb} className="uploaded-img" alt="이미지"></img>
+                </div>
+                <div className="upload-text-box">
+                    <textarea placeholder="Write a caption..."></textarea>
+                </div>
+               <button type="submit">Share</button>
+            </div>
+        </form>
     )
 }
+
+function setThumbnail(event) {
+    const reader = new FileReader();
+    reader.onload = e => {
+        let img = document.querySelector(".uploaded-img");
+        img.setAttribute("src", e.target.result);
+        
+    };
+    reader.readAsDataURL(event.target.files[0]);
+}
+
+
 
 export default Upload;
